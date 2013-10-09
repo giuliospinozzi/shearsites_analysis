e151e95dbefebc785eaaa6e05b336c13a5da767c
Giulio Spinozzi <spinozzi.giulio@hsr.it>
2013-10-09 11:08:18 +0200

    no message

---
+++ __RUN_ShearSitesIdentification.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "
+  +--------------------------------------------------------+
+  |                                                        |
+  |               Shear Site Identification                |
+  |                                                        |
+  +--------------------------------------------------------+
+  |  Author:   Giulio Spinozzi                             |
+  |  Date:     October 2013                                |
+  |  Version:  1.0                                         |  
+  |  Contact:  spinozzi.giulio@hsr.it                      |
+  +--------------------------------------------------------+
+
+  REQUIRED VARS and relative ORDER POSITION
+        1. ShearSites_identification.py 
+            (Python script to generate the Shear Sites for each couple of barcode)   
+        2. LTRXX.LCXX.shearsites.tsv 
+            (the output file of the previous python script with R1-R2 data of Shear Sites)
+        3. abundance_estimation.py 
+            (Python script to generate abundance estimation with Berry's model in R)
+"
+
+
+##### ============================ RUN INFO ============================== #####
+RUN_STARTED_AT=`date +"%Y-%m-%d %H:%M:%S"`;
+RUN_ID="`whoami`"" ${RUN_STARTED_AT}";
+#==============================================================================#
+
+
+
+##### ============== PYTHON: ShearSites_identification.py ================ #####
+for k in $(ls *.sorted.bed); do 
+  n=${k:0:10};
+  python ShearSites_identification.py --bed1 $k --bed2 $n.sorted.allr2reads.bed;
+done
+#==============================================================================#
+
+
+##### =============== BASH: output txt files for R model ================= #####
+for k in $(ls *.shearsites.tsv); do 
+  awk '{print $2" "$3" "$6"\t"$12}' $k | sort | uniq > ${k:0:21}.uniq.txt;
+done
+
+for k in $(ls *.shearsites.tsv); do 
+  awk '{print $2" "$3" "$6"\t"$12}' $k | sort > ${k:0:21}.txt;
+done
+#==============================================================================#
+
+
+##### ================ PYTHON: abundance_estimation.py =================== #####
+for k in $(ls *.shearsites.uniq.txt); do 
+  n=${k:0:10};
+  python abundance_estimation.py --dataset $k;
+done
+#==============================================================================#
---
+++ abundance_estimation.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+
+import argparse, os
+
+# Required
+import rpy2.robjects as robjects
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import sonicLength package (Rpy2 2.1+)
+from rpy2.robjects.packages import importr
+sonicLength = importr("sonicLength")
+
+# Uncomment for Rpy2 2.0
+# from rpy2.robjects import r
+# r.library("sonicLength")
+
+
+header = """
+
++--------------------------------------------------+
+ Author:	Stefano Brasca, Giulio Spinozzi
+ Date:		October 2013
+ Contact:	brasca.stefano, spinozzi.giulio @hsr.it
+ Version:	2.0
++--------------------------------------------------+
+
+ Description
+  - This is a wrapper with rpy2 for simulate the Berry's model in R.
+  
+ Note:
+  - The script needs the dataset file (LTR60.LC90.shearsites.uniq.txt)
+
+ Steps
+	1. Loads the input file
+	2. Calls the Berry's Model in R
+	3. Saves the output of the model in Python structures
+	4. Gives the output files with Phi and Theta
+""" 
+
+description = "This is a wrapper with rpy2 for simulate the Berry's model in R."
+
+usage_example = """
+Examples of usage:
+APP --in LTR60.LC90.shearsites.uniq.txt
+"""
+
+# print header
+
+parser = argparse.ArgumentParser(usage = usage_example, epilog = "[ hSR-TIGET - Vector Integration Core - Bioinformatics ] \n", description = description)
+parser.add_argument('--dataset', dest="dataset_file", help="Dataset file to process in R. No default option.", action="store", required=True)
+args = parser.parse_args()
+
+
+
+#########################################################################################
+### FUNCTIONS
+#########################################################################################
+
+def checkArgs(args):
+	"""
+	Check file path
+	"""
+	if not os.path.isfile(args.dataset_file):
+		print "\n[AP]\tError while reading files: no valid paths.\n\tExit\n"
+		sys.exit()
+
+
+def generateDataset(data):
+	# Load dataset
+	dataset_file = open(data, "r")
+	dataset = dataset_file.readlines()
+	dataset_file.close()
+
+	locations_list = []
+	length_list = []
+	for row in dataset:
+		row = row.rstrip('\n')
+		row_split = row.split("\t")
+		locations_list.append(row_split[0])
+		length_list.append(row_split[1])
+
+	return locations_list, length_list
+
+
+def plotPhi(length_phi,freq_phi):
+	length_phi_numbers = []
+	for num in length_phi:
+		length_phi_numbers.append(int(num))
+
+	# generate plot
+	plt.bar(length_phi_numbers, freq_phi, width=1.0, bottom=None, hold=True)
+	fileName = "phi.pdf"
+   	plt.savefig(fileName, format="pdf")
+   	plt.show()
+
+	# Save the figure in a separate file
+	#plt.savefig('phi_distribution.png')
+
+
+
+#########################################################################################
+### MAIN
+#########################################################################################
+
+def main():
+	"""
+	Main part of the program.
+	"""
+	#first check args and file paths
+	checkArgs(args)
+	print "[AP]\tChecked inputs, now acquiring data"
+
+	data = args.dataset_file
+
+	locations_list, length_list = generateDataset(data)
+
+	# Alias for estAbund calling
+	estAbund = sonicLength.estAbund
+
+	# Call estAbund and store returned object in results
+	results = estAbund(robjects.StrVector(locations_list), robjects.FloatVector(length_list))
+
+	# dev print
+	#print (results.r_repr())
+
+	# Put estimation for theta in estimations_theta and associated locations in locations_theta
+	theta = results.rx2("theta")
+	estimations_theta = tuple(theta)
+	locations_theta = tuple(theta.names)
+
+	# Put different fragment lengths in length_phi and associated frequencies in freq_phi
+	phi = results.rx2("phi")
+	freq_phi = tuple(phi)
+	length_phi =  tuple(phi.names)
+
+	plotPhi(length_phi,freq_phi)
+
+	print "\n[AP]\tTask Finished, closing.\n"
+
+
+# sentinel
+if __name__ == "__main__":
+    main()
